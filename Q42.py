d0b841986d37146d46d1eb8a0a04aa58a07b97e6
amiloserny <116841799+amiloserny@users.noreply.github.com>
2022-10-28 13:10:21 -0400

    Add files via upload

---
+++ Q42.py
@@ -0,0 +1,12 @@
+#Amanda Miloserny
+string = " I # love # to # go # on walks"
+def hash_spam (string, hashtag):
+    index = 0
+    while index < len(string):
+        if string >=  4:
+            print("This tweet will be considered as a spam!")
+        else:
+            return None
+    return total
+
+print(hash_spam(string, "#"))
